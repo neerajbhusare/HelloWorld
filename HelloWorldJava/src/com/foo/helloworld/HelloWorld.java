cf29677926b116d7935eeaf726c4d821c41722e6
Neeraj Bhusare <neerajbhusare@gmail.com>
2013-02-09 00:35:45 +0530

    Initial Commit
    Signed-off-by: Neeraj Bhusare <neerajbhusare@gmail.com>

---
+++ HelloWorldJava/src/com/foo/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.foo.helloworld;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld.main()");
+	}
+
+}
