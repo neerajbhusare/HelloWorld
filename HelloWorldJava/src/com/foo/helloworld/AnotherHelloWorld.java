77726725cabcfb6261652ebeaaa7f6313aaa3866
Neeraj Bhusare <neerajbhusare@gmail.com>
2013-02-09 00:52:49 +0530

    Just some changes...

--- HelloWorldJava/src/com/foo/helloworld/AnotherHelloWorld.java
+++ HelloWorldJava/src/com/foo/helloworld/AnotherHelloWorld.java
@@ -6,7 +6,7 @@ public class AnotherHelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World Changed message!");
+		System.out.println("Hello World Changed message! - 2");
 	}
 
 }
