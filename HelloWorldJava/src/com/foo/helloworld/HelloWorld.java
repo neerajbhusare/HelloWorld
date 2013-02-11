3ef00575101374d6eeb59c198fccb44f86951d45
Neeraj Bhusare <neerajbhusare@gmail.com>
2013-02-11 11:59:40 +0530

    test commit

--- HelloWorldJava/src/com/foo/helloworld/HelloWorld.java
+++ HelloWorldJava/src/com/foo/helloworld/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("HelloWorld.main() - 444");
+		System.out.println("HelloWorld.main()");
 	}
 
 }
